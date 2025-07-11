553586e0a218880a0c855a222ae27ec32ff87621
Ankit Sangwan <ankitsangwan0705@gmail.com>
2025-07-11 17:20:30 +0530

    final axios

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,16 @@ require('dotenv').config();
 const port = process.env.Port || 5000;
 
 //Middlewares
+app.use((req, res, next) => {
+  // Allow requests from your specific origin
+  res.setHeader('Access-Control-Allow-Origin', 'https://eplq-two.vercel.app');
+  // Or allow from any origin (less secure)
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors({origin:'*',credential:true}));
 app.use(express.json()); 
 
